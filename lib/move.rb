6db53b2b997a5ad23d28eb81e966d349d25bb54f
Ollie <annamharwood@gmail.com>
2022-03-31 15:07:15 -0500

    HOW DO I BOARD

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,4 @@
-def display_board(board)
+def display_board
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -7,3 +7,21 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_move)
+  user_move.to_i - 1
+  end
+   
+  board = [" ", " ", " "]
+  def update_array_at_with(array, index, value)
+    array[index] = value
+  end
+   
+  update_array_at_with(board, 0, "X")
+  
+  def move(board, index, value = "X")
+    update_array_at_with(board, index, value)
+  end
+  
+  #def move(board, index, value = "X")
+    #update_array_at_with(board, index, value)
+  #end
